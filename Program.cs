3268d5abbe11d07247a1d8e62fc429f149fadc4d
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-27 15:43:56 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("El Temach es mi pastor");
+
+string name = "Marco";
+
+Console.WriteLine($"This is my name: {name}");
+
+// string strings_array = ["One", "Two", "Three"];
+
+// Console.WriteLine($"Strings Array: {strings_array}")
+
