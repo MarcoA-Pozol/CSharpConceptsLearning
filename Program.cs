dfc0c5d036ad7ec6273a70540bdcf87d0f33ca5e
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-27 16:20:22 -0600

    Added: List example with a simple for-each loop.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
+using System.Collections.Generic;
 
 class Program
 {
@@ -14,9 +14,25 @@ class Program
 
 		Console.WriteLine($"This is my name: {name}");
 
-		// string strings_array = ["One", "Two", "Three"];
-
 		// Console.WriteLine($"Strings Array: {strings_array}")
 		Console.WriteLine("This is C#!!!");
+		
+		// Create a list of numbers and add numbers
+		List<int> numbers_list = new List<int>();
+		
+		numbers_list.Add(56);
+		numbers_list.Add(82);
+		numbers_list.Add(35);
+		numbers_list.Add(90);
+		numbers_list.Add(12);
+		
+		// For each loop
+		foreach (int number in numbers_list)
+        {
+            Console.WriteLine(number);
+        }
+		
+		// Printing a list
+		Console.WriteLine($"\nList of numbers: {numbers_list}");
 	}
 }
